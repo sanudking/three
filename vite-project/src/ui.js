fe6ff6b8f99d1c8361d736496d8293a8e86ea81d
Sudhanshu Kumar <72545992+sanudking@users.noreply.github.com>
2026-06-23 14:05:15 +0530

    lilgui align

--- vite-project/src/ui.js
+++ vite-project/src/ui.js
@@ -2,7 +2,32 @@ import {GUI} from "three/addons/libs/lil-gui.module.min.js";
 
 export function setupUI(world) {
     
-    const gui = new GUI();
+const gui = new GUI();
+
+const style = document.createElement("style");
+    style.textContent = `
+    .lil-gui {
+        background: rgba(0,0,0,0.15) !important;
+        backdrop-filter: blur(10px);
+    }
+
+    .lil-gui .title,
+    .lil-gui .controller,
+    .lil-gui .children,
+    .lil-gui input,
+    .lil-gui button {
+        background: transparent !important;
+    }
+
+    .lil-gui {
+        right: 10px !important;
+        left: auto !important;
+        bottom: 10px !important;
+        top: auto !important;
+    }
+    `;
+    document.head.appendChild(style);
+
 
     gui.add(world.size,'width',8,128,1).name("Width");
     gui.add(world.size,'height',8,128,1).name("Height");
